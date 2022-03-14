2e2cf42b7e37cbbb3ec0bc4b4bc4b40e6de65f5c
Arun Isaac <arunisaac@systemreboot.net>
2022-03-14 10:29:29 +0530

    gn: tissue: Update to 0.1.0-0.e62965d.
    * gn/packages/gemini.scm (skribilo-with-gemtext-reader): New variable.
    (tissue): Update to 0.1.0-0.e62965d.
    [arguments]: Add "GUILE_AUTO_COMPILE=0" to #:make-flags.
    [propagated-inputs]: Add skribilo-with-gemtext-reader.

--- gn/packages/gemini.scm
+++ gn/packages/gemini.scm
@@ -1,31 +1,61 @@
 (define-module (gn packages gemini)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages gettext)
   #:use-module (gnu packages guile)
+  #:use-module ((gnu packages skribilo) #:prefix guix:)
   #:use-module (guix build-system gnu)
   #:use-module (guix packages)
   #:use-module (guix git-download)
   #:use-module ((guix licenses) #:prefix license:))
 
+(define-public skribilo-with-gemtext-reader
+  (let ((commit "36a4f2945c353b432866196466da612f0a627399")
+        (revision "0"))
+    (package
+      (inherit guix:skribilo)
+      (name "skribilo")
+      (version (git-version "0.9.5" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://git.systemreboot.net/skribilo")
+                      (commit "gemtext-reader")))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "04d2xqrxfwkkljs3flixlxgcn68a9lgkzm66lr0n9hg43bhcqw8b"))))
+      (native-inputs
+       `(("autoconf" ,autoconf)
+         ("automake" ,automake)
+         ("gettext" ,gnu-gettext)
+         ,@(package-native-inputs guix:skribilo))))))
+
 (define-public tissue
-  (package
-    (name "tissue")
-    (version "0.1.0")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                    (url "https://git.systemreboot.net/tissue")
-                    (commit "20af13095723a44f58720670f3e98f04f7bf50b6")))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "0rahq7adhlx9krs31im2x3lsm0307xw12865nik2nwccqmg171k1"))))
-    (build-system gnu-build-system)
-    (arguments
-     `(#:make-flags (list (string-append "prefix=" %output))
-       #:phases
-       (modify-phases %standard-phases
-         (delete 'configure))))
-    (inputs (list guile-3.0))
-    (home-page "https://tissue.systemreboot.net")
-    (synopsis "Text based issue tracker")
-    (description "tissue is a text based issue tracker.")
-    (license license:gpl3+)))
+  (let ((commit "e62965d54e3251c737202819fc031ba4bfc55596")
+        (revision "0"))
+    (package
+      (name "tissue")
+      (version (git-version "0.1.0" revision commit))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://git.systemreboot.net/tissue")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "1vh8jfrawnymnkb04n6z7hddxgj7nfa354vra45fqckgfp5x8fb5"))))
+      (build-system gnu-build-system)
+      (arguments
+       `(#:make-flags (list (string-append "prefix=" %output)
+                            "GUILE_AUTO_COMPILE=0")
+         #:phases
+         (modify-phases %standard-phases
+           (delete 'configure))))
+      (inputs (list guile-3.0))
+      (propagated-inputs
+       (list skribilo-with-gemtext-reader))
+      (home-page "https://tissue.systemreboot.net")
+      (synopsis "Text based issue tracker")
+      (description "tissue is a text based issue tracker.")
+      (license license:gpl3+))))
