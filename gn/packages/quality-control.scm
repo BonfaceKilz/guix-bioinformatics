b030b089f26f31ff06aa14788820a2b9a25e85b7
jgart <jgart@dismail.de>
2022-04-06 11:46:55 -0400

    move sbcl-qc to quality-control module

--- gn/packages/quality-control.scm
+++ gn/packages/quality-control.scm
@@ -5,6 +5,9 @@
   #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix build-system gnu)
+  #:use-module (guix build-system asdf)
+  #:use-module (gnu packages lisp-check)
+  #:use-module (gnu packages lisp-xyz)
   #:use-module (gnu packages))
 
 (define-public libcsv
@@ -32,4 +35,47 @@
       (description
 "CSV library written in pure ANSI C that can read and write CSV data.")
       (license license:lgpl2.1+))))
+
+(define-public sbcl-qc
+  (let ((commit "ed4254445b7dc247fa27d9189fd03841ad74f568")
+        (revision "3"))
+    (package
+      (name "sbcl-qc")
+      (version (git-version "20220331" revision commit))
+      (source
+        (origin
+          (method git-fetch)
+          (uri (git-reference
+                (url "https://git.genenetwork.org/jgart/qc")
+                (commit commit)))
+          (sha256
+           (base32 "12i9s1ipqarvq6gsjqycfm8d3ym7dv7vn3nxb5c6214xrd4mmzjj"))
+          (file-name (git-file-name name commit))))
+      (build-system asdf-build-system/sbcl)
+      (arguments
+        `(#:tests? #f ; Need to fix tests.
+          #:asd-files '("qc.asd")))
+      (native-inputs
+        (list sbcl-1am))
+      (inputs
+        (list sbcl-alexandria
+              sbcl-split-sequence))
+      (home-page "https://git.genenetwork.org/jgart/qc/")
+      (synopsis "Quality control of delimited files")
+      (description
+"@code{cl-qc} is a Common Lisp library for linting the following in
+delimited text files:
+@itemize
+@item Whitespace
+@item Floating-point number well-formedness
+@item GeneChip Platform ID checks for Affymetrix, Illumina, and others
+@end itemize\n")
+      (license license:llgpl))))
+
+(define-public cl-qc
+  (sbcl-package->cl-source-package sbcl-qc))
+
+(define-public ecl-qc
+  (sbcl-package->ecl-package sbcl-qc))
+
 
