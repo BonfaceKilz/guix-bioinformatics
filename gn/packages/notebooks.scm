5a37c0ac67cf968a75d287f9e503d67003c79a86
jgart <jgart@dismail.de>
2022-04-14 22:47:20 -0400

    notebooks: add cl-nb

--- gn/packages/notebooks.scm
+++ gn/packages/notebooks.scm
@@ -42,3 +42,43 @@ instance.")
          (home-page "https://git.genenetwork.org/jgart/nb-upload/")
          (license license:unlicense))))
 
+(define-public sbcl-nb
+  (let ((commit "bb9bb6cd2ac8e9aac48e97a4c68b4b9811aa817a")
+        (revision "0"))
+    (package
+      (name "sbcl-nb")
+      (version (git-version "20220414" revision commit))
+      (source
+        (origin
+          (method git-fetch)
+          (uri (git-reference
+                (url "https://git.genenetwork.org/jgart/nb")
+                (commit commit)))
+          (sha256
+           (base32 "03wnfb2g9b9kawd9iw5hsdkzdlj0r7pkzwshhjiwn1cc04rqdi5m"))
+          (file-name (git-file-name name commit))))
+      (build-system asdf-build-system/sbcl)
+      (arguments
+        `(#:tests? #f ; There are no tests.
+          #:asd-files '("nb.asd")))
+      (inputs
+        (list sbcl-ningle
+              sbcl-clack
+              sbcl-drakma
+              sbcl-find-port
+              sbcl-jzon
+              sbcl-legit
+              sbcl-cl-interpol))
+      (home-page "https://git.genenetwork.org/jgart/nb/")
+      (synopsis "Launching interactive notebooks in GNU Guix containers")
+      (description
+"{cl-nb} is a Common Lisp system and API for launching interactive
+notebooks in GNU Guix containers.")
+      (license license:unlicense))))
+
+(define-public cl-nb
+  (sbcl-package->cl-source-package sbcl-nb))
+
+(define-public ecl-nb
+  (sbcl-package->ecl-package sbcl-nb))
+
