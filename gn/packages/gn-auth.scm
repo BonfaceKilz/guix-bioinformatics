4b2e6de82f5b31d40e519d8ec3644fe8747862fa
Frederick Muriuki Muriithi <fredmanglis@gmail.com>
2023-08-25 09:09:22 +0300

    gn-auth: New package

---
+++ gn/packages/gn-auth.scm
@@ -0,0 +1,67 @@
+(define-module (gn packages gn-auth)
+  #:use-module (guix)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system python)
+  #:use-module ((guix licenses) #:prefix license:)
+
+  ;; Packages from guix
+  #:use-module (gnu packages check)
+
+  #:use-module (gnu packages django)
+
+  #:use-module (gnu packages python-web)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages python-check)
+  #:use-module (gnu packages python-crypto)
+
+  #:use-module (gnu packages databases)
+
+  ;; Packages from guix-bioinformatics
+  #:use-module (gn packages python-web))
+
+(define-public gn-auth
+  (package
+    (name "gn-auth")
+    (version "1.0.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+	     (url "https://github.com/genenetwork/gn-auth.git")
+	     (commit "a183445c6ecf7e99aee82a2d729378b79bed58e7")))
+       (hash
+	(content-hash
+	 (base32
+	  "053h4w705v8dai3xj3qhj54d8am39w8x504y4gvbrdyczkl2fxci")))))
+    (build-system python-build-system)
+    (arguments
+     `(#:phases
+       #~(modify-phases %standard-phases
+	   (replace 'check
+	     (lambda* (#:key tests? #:allow-other-keys)
+	       (when tests?
+		 (invoke "pytest" "-k" "unit_test")))))))
+    (native-inputs
+     (list python-mypy
+	   python-pytest
+	   python-pylint
+	   python-hypothesis
+	   python-pytest-mock
+           python-mypy-extensions))
+    (propagated-inputs
+     (list gunicorn
+	   python-flask
+	   python-redis
+	   python-authlib
+	   python-pymonad
+	   yoyo-migrations
+	   python-bcrypt ;; remove after removing all references
+	   python-mysqlclient
+	   python-argon2-cffi
+	   python-email-validator))
+    (home-page "https://github.com/genenetwork/gn-auth")
+    (synopsis "Authentication and Authorisation server for GeneNetwork services")
+    (description "Authentication and Authorisation server for GeneNetwork services.")
+    (license license:agpl3+)))
